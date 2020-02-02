f8ddc91dd01de7b28aacb85dafa1ac413979903e
Sidik Permana <sidikp826@gmail.com>
2020-02-02 19:07:30 +0700

    Ubah Icon

--- application/views/login.php
+++ application/views/login.php
@@ -13,7 +13,7 @@
           <input type="text" name="username" class="form-control" placeholder="Username">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+              <span class="fas fa-user"></span>
             </div>
           </div>
         </div>
